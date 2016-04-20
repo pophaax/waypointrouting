330ef19b618cc13a43c7eda81a79996f0218a8ff
Emil Pettersson <emil.pettersson@ha.ax>
2016-04-20 12:27:17 +0000

    Fixed bug in sailCommand.

--- Commands.cpp
+++ Commands.cpp
@@ -7,16 +7,12 @@ Commands::Commands() :
 	m_portExtreme(-1),
 	m_closeReach(0),
 	m_running(1)
-{
-}
+{}
 
-Commands::~Commands()
-{
-}
+Commands::~Commands(){}
 
 
-double Commands::rudderCommand(double courseToSteer, double heading)
-{
+double Commands::rudderCommand(double courseToSteer, double heading) {
 	double offCourse = courseToSteer - heading;
 	double steeringValue = 0;
 
@@ -35,19 +31,21 @@ double Commands::rudderCommand(double courseToSteer, double heading)
 }
 
 
-double Commands::sailCommand(double relativeWindDirection)
-{
+double Commands::sailCommand(double relativeWindDirection) {
+	relativeWindDirection = Utility::limitAngleRange(relativeWindDirection);
+
 	if (relativeWindDirection > 180) {
-		return relativeWindDirection = 360 - relativeWindDirection;
-	} else if (relativeWindDirection < 45) {
+		relativeWindDirection = 360 - relativeWindDirection;
+	} 
+	if (relativeWindDirection < 45) {
 		return m_closeReach;
-	} else {
-		return relativeWindDirection * (m_running - m_closeReach) / (180 - 45);
 	}
+		
+	return m_closeReach + (relativeWindDirection - 45) * (m_running - m_closeReach) / 135;
+	
 }
 
 
-double Commands::runningSailCommand()
-{
+double Commands::runningSailCommand() {
 	return m_running;
 }
